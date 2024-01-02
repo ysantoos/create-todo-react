a0bfa7afa2b4b0829ce2acea10ecff43904a8fa8
yeison <yeison.garcia@techandsolve.com>
2024-01-02 18:11:15 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+      <CreateTodoButton />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/CreateTodoButton.js
@@ -0,0 +1,7 @@
+function CreateTodoButton() {
+    return (
+        <button>Crear tarea</button>
+    )
+}
+
+export {CreateTodoButton};
