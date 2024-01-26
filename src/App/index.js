8aca2b0e0fee0254d5d002036bf15e682195e69a
Yeison Garcia Santos <37028840+ysantoos@users.noreply.github.com>
2024-01-25 22:27:49 -0500

    abstraccion de componente app

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { TodoCounter } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
-import { TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem';
-import { CreateTodoButton } from '../CreateTodoButton';
-import {useLocalStorage} from './useLocalStorage';
+import { useLocalStorage } from './useLocalStorage';
+import { AppUI } from './AppUI';
 
 function App() {
 
@@ -27,19 +23,14 @@ function App() {
     newTodos.splice(todoIndex, 1); saveTodos(newTodos);
   };
 
-  return (<><TodoCounter
-    completed={completedTodos}
-    total={totalTodos} /><TodoSearch
-      searchValue={searchValue}
-      setSearchValue={setSearchValue} />
-    <TodoList>{searchedTodos.map(todo => (<TodoItem
-      key={todo.text}
-      text={todo.text}
-      completed={todo.completed}
-      onComplete={() => completeTodo(todo.text)}
-      onDelete={() => deleteTodo(todo.text)} />))}
-    </TodoList>
-    <CreateTodoButton />
-  </>);
+  return (<AppUI
+    completedTodos={completedTodos}
+    totalTodos={totalTodos}
+    searchValue={searchValue}
+    setSearchValue={setSearchValue}
+    searchedTodos={searchedTodos}
+    completeTodo={completeTodo}
+    deleteTodo={deleteTodo} />
+  )
 }
 export default App;
